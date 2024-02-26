80b066ba0d4cbefefcd379c464f932a76e375e8a
Santiago Jaimes <79803432+SantiagoJaimesG@users.noreply.github.com>
2024-02-26 16:52:35 -0500

    new branch santiago

---
+++ app/Services/Calculate/InteresCompuesto/CalculateInterestCompoundService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Services\Calculate\InteresCompuesto;
+
+
+class CalculateInterestCompoundService
+{
+    function calculate($request){
+        $data = (object)$request->all();
+        if($data->type_calculate == "MONTO COMPUESTO") return $this->calculateCompoundAmount($request);
+        if($data->type_calculate == "CAPITAL") return $this->calculateCapital($request);
+        if($data->type_calculate == "TASA DE INTERES") return $this->calculateInterestRate($request);
+        if($data->type_calculate == "TIEMPO INVERTIDO") return $this->calculateTime($request);
+    }
+
+    public function calculateCapital($request) {
+    	$time = $request->time;
+        $compoundAmount = $request->compoundAmount;
+        $interestRate = $request->interestRate / 100;
+        $factor = pow(1 + $interestRate, $time);
+        $capital = ($compoundAmount / $factor);
+        return $capital;
+    }
+
+    public function calculateCompoundAmount($request){
+        $capital = $request->capital;
+        $interestRate = $request->interestRate / 100;
+        $time = $request->time;
+        $factor = pow(1 + $interestRate, $time);
+
+        $compoundAmount = ($capital * $factor);
+
+        return $compoundAmount;
+    }
+
+    public function calculateInterestRate($request) {
+    	$time = $request->time;
+        $capital = $request->capital;
+        $compoundAmount = $request->compoundAmount;
+        $factor = pow($compoundAmount / $capital, 1 / $time) - 1;
+
+        $interestRate = $factor * 100;
+
+        return $interestRate . '%';
+    }
+
+    /*public function calculateTime($request) {
+        $capital = $request->capital;
+        $interestEarned = $request->interestEarned;
+        $interestRate = $request->interestRate / 100;
+        $time = ($interestEarned / ($capital * $interestRate));
+        $time = $time * 360;
+
+        $years = floor($time / 360);
+        $remainingDays = $time % 360;
+        $months = floor($remainingDays / 30);
+        $days = round($remainingDays % 30);
+
+        return ['years' => $years, 'months' => $months, 'days' => $days, 'time' => ($time/360)];
+    }*/
+}
--- app/Services/Calculate/OrchestratorService.php
+++ app/Services/Calculate/OrchestratorService.php
@@ -3,24 +3,36 @@
 namespace App\Services\Calculate;
 
 use App\Services\Calculate\InteresSimple\CalculateInterestSimpleService;
+use App\Services\Calculate\InteresCompuesto\CalculateInterestCompoundService;
 
 class OrchestratorService
 {
     private $calculateInteresSimpleService;
+    private $calculateInteresCompoundService;
 
-    public function __construct(CalculateInterestSimpleService $calculateInteresSimpleService){
+    public function __construct(
+        CalculateInterestSimpleService $calculateInteresSimpleService,
+        CalculateInterestCompoundService $calculateInteresCompoundService
+    ) {
         $this->calculateInteresSimpleService = $calculateInteresSimpleService;
+        $this->calculateInteresCompoundService = $calculateInteresCompoundService;
     }
 
-    function calculate($request){
-        $data = (object)$request->all();
-        if($data->type_interest == "SIMPLE")
+    public function calculate($request)
+    {
+        $data = (object) $request->all();
+        if ($data->type_interest == "SIMPLE") {
             return $this->resolve($this->calculateInteresSimpleService->calculate($request));
+        } elseif ($data->type_interest == "COMPUESTO") {
+            return $this->resolve($this->calculateInteresCompoundService->calculate($request));
+        }
     }
 
-    function resolve($data){
+    public function resolve($data)
+    {
         return [
             "result" => $data
         ];
     }
 }
+
